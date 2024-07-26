fa71c869b1db0193b4eeebabeecda47784c1b96f
TGaDev203 <hoangthuychimte@gmail.com>
2024-07-27 01:05:41 +0700

    Fix Jump Out From Ladder

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class InputManager1 : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static InputManager Instance {get; private set;} // To ensure that only one instance of this class exists and provide a global point (Singleton Instance)
+
+    private PlayerAction playerInputAction;
+    public event EventHandler OnJump;
+    
+    private void Awake()
     {
-        
+        Instance = this;
+        playerInputAction = new PlayerAction();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerInputAction.Player.Move.Enable();
+        playerInputAction.Player.Climb.Enable();
+    }
+
+    private void OnEnable()
+    {
+        playerInputAction.Player.Jump.Enable();
+        playerInputAction.Player.Jump.performed += HandleJump; // Subscribe event
+
+    }
+
+    private void HandleJump(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        OnJump?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector2 GetInputVectorMove() 
+    {
+        Vector2 inputVectorMove = playerInputAction.Player.Move.ReadValue<Vector2>();
+        return inputVectorMove.normalized;
+    }
+
+    public Vector2 GetInputVectorClimb()
+    {
+        Vector2 inputVectorClim = playerInputAction.Player.Climb.ReadValue<Vector2>();
+        return inputVectorClim.normalized;
+    }
+
+    public bool IsJumping()
+    {
+        return playerInputAction.Player.Jump.IsPressed();
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,103 +9,164 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Input Value")]
     [SerializeField] float runSpeed = 1f;
     [SerializeField] float jumpForce = 2f;
     [SerializeField] float climbSpeed = 5f;
-    private Animator playerAnimator;
     private Rigidbody2D rigidBody;
-    private Vector2 moveInput;
     private CapsuleCollider2D capsuleCollider;
-    private float gravityScaleAtStart;
-    private SpriteRenderer spriteRenderer;
-    // bool isClimbing = false;
+    private float gravityAtStart = 3f;
+    private bool isClimbing;
+    // private Animator playerAnimator;
+    // private Vector2 moveInput;
+    // private SpriteRenderer spriteRenderer;
 
-    void Start()
+    private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        playerAnimator = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
-        gravityScaleAtStart = rigidBody.gravityScale;
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        gravityAtStart = rigidBody.gravityScale;
+        // playerAnimator = GetComponent<Animator>();
+        // capsuleCollider = GetComponent<CapsuleCollider2D>();
+        // gravityScaleAtStart = rigidBody.gravityScale;
+        // spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    private void Update()
     {
-        Run();
-        FlipSprite();
-        ClimbLadder();
+        Move();
+        Climb();
+        // FlipSprite();
+        // ClimbLadder();
     }
 
-    void OnMove(InputValue value)
+    private void Start()
     {
-        moveInput = value.Get<Vector2>();
+        InputManager.Instance.OnJump += HandleJump;
     }
 
-    void OnJump(InputValue value)
+    private void OnDisable()
     {
-        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (InputManager.Instance != null)
         {
-            return;
-        }
-
-        if (value.isPressed)
-        {
-            rigidBody.velocity += new Vector2(rigidBody.velocity.x, jumpForce);
-        }
-    }
-
-    void Run()
-    {
-        bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidBody.velocity.y);
-        rigidBody.velocity = playerVelocity;
-        playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
-    }
-
-    void FlipSprite()
-    {
-        bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
-
-        if (playerHasHorizontalSpeed)
-        {
-            spriteRenderer.flipX = rigidBody.velocity.x < 0;
-            // transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
-            playerAnimator.SetBool("isRunning", true);
-        }
-
-        else
-        {
-            playerAnimator.SetBool("isRunning", false);
+            InputManager.Instance.OnJump -= HandleJump;
         }
     }
 
-    void ClimbLadder()
+    // void OnMove(InputValue value)
+    // {
+    //     moveInput = value.Get<Vector2>();
+    // }
+
+    // void OnJump(InputValue value)
+    // {
+    //     if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+    //     {
+    //         return;
+    //     }
+
+    //     if (value.isPressed)
+    //     {
+    //         rigidBody.velocity += new Vector2(rigidBody.velocity.x, jumpForce);
+    //     }
+    // }
+
+    private void Move()
     {
-        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        // bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+        // Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidBody.velocity.y);
+        // rigidBody.velocity = playerVelocity;
+        // playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        // float moveHorizontal = Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime;
+        // Vector2 position = new Vector2(moveHorizontal, 0);
+        // transform.Translate(position);
+        // Vector2 inputVector = InputManager.Instance.GetInputVectorNormalize();
+        // rigidBody.velocity = new Vector2(inputVector.x * runSpeed, rigidBody.velocity.y);
+
+        Vector2 inputVectorMove = InputManager.Instance.GetInputVectorMove();
+        rigidBody.velocity = new Vector2(inputVectorMove.x * runSpeed, rigidBody.velocity.y);
+    }
+
+    private void Climb()
+    {
+        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
         {
-            rigidBody.gravityScale = gravityScaleAtStart;
-            playerAnimator.SetBool("isClimbing", false);
-            return;
+            Vector2 inputVectorClimb = InputManager.Instance.GetInputVectorClimb();
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, inputVectorClimb.y * climbSpeed);
+            rigidBody.gravityScale = 0f;
+            isClimbing = true;
+            if (InputManager.Instance.IsJumping())
+            {
+                rigidBody.gravityScale = gravityAtStart;
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+            }
         }
 
-        // Check if the player is trying to jump
-        // if (!isClimbing)
+        else 
+        {
+            rigidBody.gravityScale = gravityAtStart;
+            isClimbing = false;
+        }        
+    }
+
+    private void HandleJump(object sender, EventArgs e)
+    {
+        // if (isClimbing)
         // {
-        // Vector2 jumpVelocity = new Vector2(rb.velocity.x, jumpForce);
-        // rb.velocity = jumpVelocity;
-        // return;
-        // rb.velocity += new Vector2(rb.velocity.x, jumpForce);
+        //     rigidBody.gravityScale = gravityAtStart;
+        //     rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
         // }
 
-        // Calculate climb velocity
-        Vector2 climbVelocity = new Vector2(rigidBody.velocity.x, moveInput.y * climbSpeed);
-        rigidBody.velocity = climbVelocity;
-
-        // Disable gravity while climbing
-        rigidBody.gravityScale = 0f;
-
-        // Update animator based on vertical speed
-        bool playerHasVerticalSpeed = Mathf.Abs(rigidBody.velocity.y) > Mathf.Epsilon;
-        playerAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) || capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Mushroom")))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
     }
+
+    // void FlipSprite()
+    // {
+    //     bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+
+    //     if (playerHasHorizontalSpeed)
+    //     {
+    //         spriteRenderer.flipX = rigidBody.velocity.x < 0;
+    //         // transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+    //         playerAnimator.SetBool("isRunning", true);
+    //     }
+
+    //     else
+    //     {
+    //         playerAnimator.SetBool("isRunning", false);
+    //     }
+    // }
+
+    // void ClimbLadder()
+    // {
+    // if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+    // {
+    //     rigidBody.gravityScale = gravityScaleAtStart;
+    //     playerAnimator.SetBool("isClimbing", false);
+    //     return;
+    // }
+
+    // Check if the player is trying to jump
+    // if (!isClimbing)
+    // {
+    // Vector2 jumpVelocity = new Vector2(rb.velocity.x, jumpForce);
+    // rb.velocity = jumpVelocity;
+    // return;
+    // rb.velocity += new Vector2(rb.velocity.x, jumpForce);
+    // }
+
+    // Calculate climb velocity
+    // Vector2 climbVelocity = new Vector2(rigidBody.velocity.x, moveInput.y * climbSpeed);
+    // rigidBody.velocity = climbVelocity;
+
+    // Disable gravity while climbing
+    // rigidBody.gravityScale = 0f;
+
+    // Update animator based on vertical speed
+    // bool playerHasVerticalSpeed = Mathf.Abs(rigidBody.velocity.y) > Mathf.Epsilon;
+    // playerAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    // }
 }
